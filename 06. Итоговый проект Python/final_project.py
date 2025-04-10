760844addb73f1d8e2fb7f12fe2f4f6330596543
Mikhail <mishandri@gmail.com>
2025-04-10 16:25:10 +0300

    Реализован класс GetData

--- 06. Итоговый проект Python/final_project.py
+++ 06. Итоговый проект Python/final_project.py
@@ -1,13 +1,21 @@
 # Импортируем необходимые библиотеки
 import requests # Для запроса данных их API
 import logging  # Для логирования
-import psycopg2 # Для работы с PostgeSQL
+import psycopg2 # Для работы с PostgreSQL
 import gspread  # Для работы с GoogleSheets
 from oauth2client.service_account import ServiceAccountCredentials #Для авторизации на гугле
 from datetime import datetime   # Для работы с датой и временем
+import ast # для чтения "кода"
 
 # Переменные
 api_url = "https://b2b.itresume.ru/api/statistics"
+params={
+    "client": "Skillfactory",
+    "client_key": "M2MGWS",
+    "start": "2023-04-01 12:46:47.860798",
+    "end": "2023-04-01 13:49:47.860798"
+}
+
 psql_address = "localhost"
 psql_port = 5432
 psql_db = "postgres"
@@ -18,13 +26,43 @@ psql_pass = 123
 class ConnectDB:
     ...
 
-#TODO Создать класс для запроса данных с удалённого сервера
 class GetData:
-    ...
+    def __init__(self, url, params):
+        self.url = url
+        self.__params = params
+        response = requests.get(self.url, params=self.__params)
+        self.status = response.status_code
+        self.__json = response.json()
+
+    def dict(self):
+        res = []
+        for el in self.__json:
+            dict_res = {}
+            dict_res['user_id'] = el['lti_user_id']
+            d = ast.literal_eval((el['passback_params']))
+            for key in ('oauth_consumer_key', 'lis_result_sourcedid', 'lis_outcome_service_url'):
+                dict_res[key] = d[key] if key in d else ""
+            dict_res['is_correct'] = el['is_correct']
+            dict_res['attempt_type'] = el['attempt_type']
+            dict_res['created_at'] = el['created_at']
+            res.append(dict_res)
+        return res
 
 #TODO Создать класс для логирования всех действий
 class Logging:
     ...
+        # def status(self):
+    #     status_code = r.status_code
+    #     if status_code == 200:
+    #         print("Запрос выполнен успешно!")
+    #     elif status_code == 404:
+    #         print("Ресурс не найден.")
+    #     elif status_code == 403:
+    #         print("Доступ запрещён!")
+    #     elif status_code == 503:
+    #         print("Сервер не доступен...")
+    #     else:
+    #         print("Произошла ошибка:", status_code)
 
 #TODO Создать класс для отправки сообщений по электронной почте
 class Email:
@@ -33,3 +71,8 @@ class Email:
 #TODO Создать класс для записи данных в Google Sheets
 class GoogleSheet:
     ...
+
+# Функция обращается к серверу по API и возвращает словарь с данными
+data = GetData(api_url, params).dict()
+
+print(data)
