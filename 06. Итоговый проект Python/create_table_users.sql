58b8e68b337f0f3ee0dea0f8864168f90eadb14a
Mikhail <mishandri@gmail.com>
2025-04-10 20:22:15 +0300

    SQL для создания таблицы в БД

---
+++ 06. Итоговый проект Python/create_table_users.sql
@@ -0,0 +1,9 @@
+CREATE TABLE users (
+    user_id VARCHAR NOT NULL,
+    oauth_consumer_key VARCHAR NOT NULL,
+    lis_result_sourcedid VARCHAR,
+    lis_outcome_service_url VARCHAR,
+    is_correct VARCHAR(1),  -- Например, "1"/"0" или Null
+    attempt_type VARCHAR(6), -- Например, "run"/"submit"
+    created_at TIMESTAMP
+);
