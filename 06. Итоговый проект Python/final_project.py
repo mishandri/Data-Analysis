07bcc89a8e72742fa578c1de8bab304139994b6f
Mikhail <mishandri@gmail.com>
2025-04-10 11:58:54 +0300

    + общие переменные, + import psycopg2

--- 06. Итоговый проект Python/final_project.py
+++ 06. Итоговый проект Python/final_project.py
@@ -1,9 +1,18 @@
 # Импортируем необходимые библиотеки
 import requests # Для запроса данных их API
 import logging  # Для логирования
+import psycopg2 # Для работы с PostgeSQL
 import gspread  # Для работы с GoogleSheets
 from oauth2client.service_account import ServiceAccountCredentials #Для авторизации на гугле
 from datetime import datetime   # Для работы с датой и временем
+
+# Переменные
+api_url = "https://b2b.itresume.ru/api/statistics"
+psql_address = "localhost"
+psql_port = 5432
+psql_db = "postgres"
+psql_user = "postgres"
+psql_pass = 123
 
 #TODO Создать класс-синглтон для подключения к локальной БД
 class ConnectDB:
