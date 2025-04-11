6e4dbdc9732b448c6b85e06d49a367d1382b5c00
Mikhail <mishandri@gmail.com>
2025-04-11 19:28:13 +0300

    Основное задание выполнено

--- 06. Итоговый проект Python/final_project.py
+++ 06. Итоговый проект Python/final_project.py
@@ -1,29 +1,37 @@
 # Импортируем необходимые библиотеки
-import requests # Для запроса данных их API
+import requests  # Для запроса данных их API
 import logging  # Для логирования
+from logging.handlers import TimedRotatingFileHandler
 from pathlib import Path
-import psycopg2 # Для работы с PostgreSQL
+import psycopg2  # Для работы с PostgreSQL
 import gspread  # Для работы с GoogleSheets
-from oauth2client.service_account import ServiceAccountCredentials #Для авторизации на гугле
-from datetime import datetime   # Для работы с датой и временем
-import ast # для чтения "кода", чтобы быстро строку превратить в словарь
-today = datetime.now().strftime("%Y-%m-%d")
+# Для авторизации на гугле
+from oauth2client.service_account import ServiceAccountCredentials
+from datetime import datetime, timedelta   # Для работы с датой и временем
+import ast  # для чтения "кода", чтобы быстро строку превратить в словарь
+import os
 
-#Настройка логера:
+# Считаем, что код запускается где-то ночью и записывает данные в БД за вчерашний день timedelta(days=1)
+yesterday = (datetime.now() - timedelta(days=1)).strftime("%Y-%m-%d")
+start = f'{yesterday} 00:00:00.000000'
+end = f'{yesterday} 23:59:59.999999'
+
+# Настройка логера:
 logging.basicConfig(
-    format='%(asctime)s %(levelname)s: %(message)s"',
+    format='%(asctime)s %(levelname)s: %(message)s',
     level=logging.INFO,
     encoding='utf-8',
-    filename=f"{Path(__file__).parent}/{today}.log",
-    filemode="a")
-
+    filename=f"{Path(__file__).parent}/{yesterday}.log",
+    filemode="a"
+)
+logging.info("Запуск программы...")
 # Переменные
-api_url = "https://b2b.itresume.ru/api/"#statistics"
-params={
+api_url = "https://b2b.itresume.ru/api/statistics"
+params = {
     "client": "Skillfactory",
     "client_key": "M2MGWS",
-    "start": "2023-04-01 12:46:47.860798",
-    "end": "2023-04-01 12:49:47.860798"
+    "start": start,
+    "end": end
 }
 
 psql_address = "localhost"
@@ -31,6 +39,7 @@ psql_port = 5432
 psql_db = "postgres"
 psql_user = "postgres"
 psql_pass = 123
+
 
 class ConnectDB:
     __instance = None  # Явное объявление приватного атрибута класса
@@ -42,11 +51,12 @@ class ConnectDB:
         return ConnectDB.__instance
 
     def __init__(self):
+        """Устанавливает соединение с БД"""
         if ConnectDB.__instance:
-            logging.warning(f"Попытка создать второй экземпляр класса {self.__class__.__name__}")
+            logging.warning(
+                f"Попытка создать второй экземпляр класса {self.__class__.__name__}")
             raise Exception("Этот класс является Singleton")
         else:
-            print(f'{self.__class__}')
             self.connection = psycopg2.connect(
                 host=psql_address,
                 port=psql_port,
@@ -55,12 +65,21 @@ class ConnectDB:
                 password=psql_pass
             )
             ConnectDB.__instance = self
-            logging.info(f'Соединение с БД "{psql_db}:{psql_port}" открыто')
+            logging.info(
+                f'Соединение с БД "{psql_db}:{psql_port}" установлено')
+
+    def commit(self):
+        self.connection.commit()
+
+    def rollback(self):
+        self.connection.rollback()
 
     def close_instance(self):
+        """Закрывает соединение с БД"""
         if ConnectDB.__instance:
             self.connection.close()
             logging.info(f'Соединение с БД "{psql_db}:{psql_port}" закрыто')
+
 
 class APIClient:
     def __init__(self, url, params):
@@ -73,6 +92,7 @@ class APIClient:
             response = requests.get(self.url, params=self.__params)
             self.status = response.status_code
             self.__json = response.json()
+            logging.info('Данные из API получены')
         except requests.exceptions.HTTPError:
             logging.warning(f"HTTP Error: {self.status}")
             return self.status
@@ -80,33 +100,88 @@ class APIClient:
             logging.warning(f"Request Error: {self.status}")
             return self.status
         res = []
-        for el in self.__json:
-            dict_res = {}
-            dict_res['user_id'] = el['lti_user_id']
-            d = ast.literal_eval((el['passback_params']))
-            for key in ('oauth_consumer_key', 'lis_result_sourcedid', 'lis_outcome_service_url'):
-                dict_res[key] = d[key] if key in d else ""
-            dict_res['is_correct'] = el['is_correct']
-            dict_res['attempt_type'] = el['attempt_type']
-            dict_res['created_at'] = el['created_at']
-            res.append(dict_res)
-        return res
+        try:
+            logging.info('Начата обработка данных')
+            for el in self.__json:
+                dict_res = {}
+                dict_res['user_id'] = el['lti_user_id']
+                if 'passback_params' in el:
+                    if el['passback_params'] is not None:
+                        d = ast.literal_eval((el['passback_params']))
+                    else:
+                        d = {}
+                    for key in ('oauth_consumer_key', 'lis_result_sourcedid', 'lis_outcome_service_url'):
+                        dict_res[key] = d[key] if key in d else ""
+                else:
+                    dict_res['oauth_consumer_key'] = ""
+                    dict_res['lis_result_sourcedid'] = ""
+                    dict_res['lis_outcome_service_url'] = ""
+                dict_res['is_correct'] = el['is_correct']
+                dict_res['attempt_type'] = el['attempt_type']
+                dict_res['created_at'] = el['created_at']
+                for k, v in dict_res.items():
+                    if v == "":
+                        logging.warning(
+                            f'Обнаружено пустое значение в ключе {k}')
+                res.append(dict_res)
+            logging.info('Обработка данных закончена')
+            return res
+        except KeyError:
+            logging.error('Ошибка ключа при формировании словаря')
+            return []
 
-#TODO Создать класс для логирования всех действий
-class Logging:
-    ...
+# TODO Создать класс для отправки сообщений по электронной почте
 
-#TODO Создать класс для отправки сообщений по электронной почте
+
 class Email:
     ...
 
-#TODO Создать класс для записи данных в Google Sheets
+# TODO Создать класс для записи данных в Google Sheets
+
+
 class GoogleSheet:
     ...
 
 
+# Получаем данные из API
 data = APIClient(api_url, params).fetch_data()
-print(data)
-db_connection = ConnectDB.get_instance()
-print(db_connection)
-db_connection.close_instance()
+
+db_connection = ConnectDB.get_instance()  # Соединяемся с БД
+try:
+    cursor = db_connection.connection.cursor()  # Устанавливаем курсор
+    query = """
+    INSERT INTO users (
+        user_id, 
+        oauth_consumer_key, 
+        lis_result_sourcedid, 
+        lis_outcome_service_url, 
+        is_correct, 
+        attempt_type, 
+        created_at
+        ) 
+        VALUES (
+        %(user_id)s, 
+        %(oauth_consumer_key)s, 
+        %(lis_result_sourcedid)s,
+        %(lis_outcome_service_url)s,
+        %(is_correct)s,
+        %(attempt_type)s,
+        %(created_at)s
+        )
+    """
+    cursor.executemany(query, data)  # Передаём список словарей для вставки многих строк
+    db_connection.commit()  # "Подтверждаем" вставку
+    logging.info(f'Данные успешно занесены в БД')
+except Exception as e:
+    logging.error(f"Неожиданная ошибка: {e}")
+    if db_connection:
+        # "Отменяем" вставку
+        db_connection.rollback()
+finally:
+    if db_connection:
+        # Отсоединяемся от БД
+        db_connection.close_instance()
+
+
+# yesterday = (datetime.now() - timedelta(days=732)).strftime("%Y-%m-%d")
+logging.info("Работа программы успешно завершена")
