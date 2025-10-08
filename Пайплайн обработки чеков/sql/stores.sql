2aafac74cc0ec4f773ffac50e31b8d65f4153c1d
mishandri <mishandri@gmail.com>
2025-10-08 23:39:16 +0300

    SQL для названий магазинов

---
+++ Пайплайн обработки чеков/sql/stores.sql
@@ -0,0 +1,18 @@
+CREATE TABLE stores (
+    store_id INT PRIMARY KEY,
+    store_name VARCHAR(50) NOT NULL
+);
+
+INSERT INTO stores (store_id, store_name) VALUES
+(1, 'Олимп'),
+(2, 'Амфора'),
+(3, 'Зевс'),
+(4, 'Руно'),
+(5, 'Лавр'),
+(6, 'Гармония'),
+(7, 'Афродита'),
+(8, 'Агора'),
+(9, 'Эллада'),
+(10, 'Ития'),
+(11, 'Сиртаки'),
+(12, 'Киприда');
